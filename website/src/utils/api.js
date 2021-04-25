34830f72b70e7afff99f86f0233e95a4d84a1089
Nano-Stark <inventorstark7@gmail.com>
2021-04-25 02:51:45 +0100

    fifth commit

--- website/src/utils/api.js
+++ website/src/utils/api.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 //comment out when to deploy
 //const host = 'http://localhost:8080';
+const host = "https://stark-website.herokuapp.com/";
 
-/*USE THIS IN DEPLOYMENT*/
+/*USE THIS IN DEPLOYMENT*//*
 if (process.env.NODE_ENV === 'development'){
     host = 'http://localhost:8080';
 }else{
     host = 'https://stark-website.herokuapp.com/';//host address
-}
+}*/
 
 const API  = {
     makeFileURL: (url, token) => {
