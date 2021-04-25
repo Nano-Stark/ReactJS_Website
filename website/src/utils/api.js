e42313fd725e371d84dcf832493e11663d015283
Nano-Stark <inventorstark7@gmail.com>
2021-04-25 02:08:29 +0100

    fifth commit

--- website/src/utils/api.js
+++ website/src/utils/api.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 if (process.env.NODE_ENV === 'development'){
     host = 'http://localhost:8080';
 }else{
-    host = 'https://powerful-citadel-92824.herokuapp.com/';//host address
+    host = 'https://stark-website.herokuapp.com/';//host address
 }
 
 const API  = {
