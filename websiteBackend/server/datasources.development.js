bbd6e6152b758c72ee229b46f4be2ee9eb661d4e
Nano-Stark <inventorstark7@gmail.com>
2021-05-01 12:43:08 +0100

    edited commit

--- websiteBackend/server/datasources.development.js
+++ websiteBackend/server/datasources.development.js
@@ -9,20 +9,6 @@ module.exports = {
         "user": ""
       }
 }
-
-//i copied this from datasources.js, was just trying to confirm if it works by altering my local host
-//and it worked
-/**
-"db": {
-      "host": "localhost",
-      "port": 27017,
-      "database": "mywebsite",
-      "password": "",
-      "name": "db",
-      "connector": "mongodb",
-      "user": ""
-    },
- */
 /*
    "loopback#static": {
       "params": "$!../client"
--- websiteBackend/server/datasources.production.js
+++ websiteBackend/server/datasources.production.js
@@ -11,16 +11,3 @@ module.exports = {
         "connector": "mongodb"
         }
 }
-//COPIED FROM datasourc.prod.json to allow this code and allow env var after installing dotenv
-//and putting it into servere.js file via require function
-/*    "db": {
-        "host": "reactjs-website-cluster.9xbvj.mongodb.net",
-        "port": 27017,
-        "url": "mongodb+srv://stark:stark70@reactjs-website-cluster.9xbvj.mongodb.net/mywebsite?retryWrites=true&w=majority",
-        "database": "mywebsite",
-        "password": "stark70",
-        "name": "db",
-        "user": "stark",
-        "useNewUrlParser": true,
-        "connector": "mongodb"
-        } */
